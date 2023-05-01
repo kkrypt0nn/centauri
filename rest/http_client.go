c311aa6921c4c169dfc66b33ae1318f28fb003b5
Krypton <root@krypton.ninja>
2023-05-01 19:48:56 +0200

    feat: Added interceptor for HTTP requests (#4)

---
+++ rest/http_client.go
@@ -0,0 +1,31 @@
+package rest
+
+import "net/http"
+
+// HttpClient is a http.Client extended with a custom interceptor
+type HttpClient struct {
+	*http.Client
+	Interceptor Interceptor
+}
+
+// SetInterceptor sets the interceptor for the HTTP client
+func (c *HttpClient) SetInterceptor(interceptor Interceptor) {
+	c.Interceptor = interceptor
+}
+
+// Interceptor is an interface all custom interceptor should inherit
+type Interceptor interface {
+	ModifyRequest(r *http.Request)
+	ModifyResponse(r *http.Response)
+}
+
+// DefaultInterceptor is a default interceptor so that you do not have to add both methods in your custom interceptor
+type DefaultInterceptor struct {
+	Interceptor
+}
+
+// ModifyRequest is the default request modifying method from the default interceptor, which does nothing
+func (i *DefaultInterceptor) ModifyRequest(_ *http.Request) {}
+
+// ModifyResponse is the default response modifying method from the default interceptor, which does nothing
+func (i *DefaultInterceptor) ModifyResponse(_ *http.Response) {}
