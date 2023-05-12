ed9d43c9a1fe64d6623d7663341b5863fe31b344
Krypton <root@krypton.ninja>
2023-05-12 14:35:29 +0200

    feat: Added support for channel related endpoints

---
+++ discord/invite.go
@@ -0,0 +1,32 @@
+package discord
+
+import "time"
+
+type Invite struct {
+	Code                     string               `json:"code"`
+	Guild                    *PartialGuild        `json:"guild,omitempty"`
+	Channel                  *Channel             `json:"channel,omitempty"`
+	Inviter                  *User                `json:"inviter,omitempty"`
+	TargetType               InviteTargetType     `json:"target_type,omitempty"`
+	TargetUser               *User                `json:"target_user,omitempty"`
+	TargetApplication        *Application         `json:"target_application,omitempty"`
+	ApproximatePresenceCount int                  `json:"approximate_presence_count"`
+	ApproximateMemberCount   int                  `json:"approximate_member_count"`
+	ExpiresAt                time.Time            `json:"expires_at"`
+	StageInstance            *InviteStageInstance `json:"stage_instance,omitempty"`
+	GuildScheduledEvent      *GuildScheduledEvent `json:"guild_scheduled_event,omitempty"`
+}
+
+type InviteTargetType int
+
+const (
+	InviteTargetTypeStream InviteTargetType = 1 + iota
+	InviteTargetTypeEmbeddedApplication
+)
+
+type InviteStageInstance struct {
+	Members          []Member `json:"members"`
+	ParticipantCount int      `json:"participant_count"`
+	SpeakerCount     int      `json:"speaker_count"`
+	Topic            string   `json:"topic"`
+}
--- rest/channel.go
+++ rest/channel.go
@@ -1,6 +1,9 @@
 package rest
 
-import "github.com/kkrypt0nn/centauri/discord"
+import (
+	"github.com/kkrypt0nn/centauri/discord"
+	"strconv"
+)
 
 const (
 	ChannelsEndpoint = Endpoint + "channels"
@@ -19,4 +22,31 @@ func (c *Client) GetChannelMessages(channelID string) ([]discord.Message, error)
 // GetChannelMessage returns a discord.Message given its ID
 func (c *Client) GetChannelMessage(channelID, messageID string) (*discord.Message, error) {
 	return DoRequestAs[discord.Message](c, "GET", ChannelsEndpoint+"/"+channelID+"/messages/"+messageID, nil, 1)
+}
+
+// GetReactions returns a list of discord.User that reacted to the message with the given emoji
+func (c *Client) GetReactions(channelID, messageID, emoji, after string, limit int) ([]discord.User, error) {
+	queryParams := make(QueryParameters)
+	if after != "" {
+		queryParams["after"] = after
+	}
+	if limit >= 1 && limit <= 100 {
+		queryParams["limit"] = strconv.Itoa(limit)
+	}
+	return DoRequestAsList[discord.User](c, "GET", ChannelsEndpoint+"/"+channelID+"/messages/"+messageID+"/reactions/"+emoji, queryParams, 1)
+}
+
+// GetChannelInvites returns a list of discord.Invite with discord.InviteMetadata that are available in the specific channel
+func (c *Client) GetChannelInvites(channelID string) ([]discord.Invite, error) {
+	return DoRequestAsList[discord.Invite](c, "GET", ChannelsEndpoint+"/"+channelID+"/invites", nil, 1)
+}
+
+// GetPinnedMessages returns a list of discord.Message that are pinned in the given channel
+func (c *Client) GetPinnedMessages(channelID string) ([]discord.Message, error) {
+	return DoRequestAsList[discord.Message](c, "GET", ChannelsEndpoint+"/"+channelID+"/pins", nil, 1)
+}
+
+// GetThreadMember returns the discord.ThreadMember if they are a member of the thread
+func (c *Client) GetThreadMember(threadID, userID string) (*discord.ThreadMember, error) {
+	return DoRequestAs[discord.ThreadMember](c, "GET", ChannelsEndpoint+"/"+threadID+"/thread-members/"+userID, nil, 1)
 }
