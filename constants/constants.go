c3d853c36713b42f071b45acd5cff20a6681d36d
Krypton <root@krypton.ninja>
2023-06-12 15:12:09 +0200

    chore: Development version bump

--- constants/constants.go
+++ constants/constants.go
@@ -5,5 +5,5 @@ const (
 	// GitHubURL is the URL to the GitHub repository, used for the user agent when doing requests to Discord's API
 	GitHubURL = "https://github.com/kkrypt0nn/centauri"
 	// Version is the library's version that follows the Semantic Versioning Specification (https://semver.org)
-	Version = "0.4.0"
+	Version = "0.5.0a"
 )
